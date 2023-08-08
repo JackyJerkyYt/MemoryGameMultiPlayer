8b618acbdecf6828855b6cb406829050e96bb006
JackyJerkyYt <jackywongcheukki@gmail.com>
2023-08-07 23:09:40 -0700

    added url to fetch the top  scorers

--- NewestGoldFish_Backend/server/routes/routing.js
+++ NewestGoldFish_Backend/server/routes/routing.js
@@ -1,42 +1,55 @@
-const router = require('express').Router();
-let Score = require('../models/score.model')
+const router = require("express").Router();
+let Score = require("../models/score.model");
 
-router.route('/user').get((req, res) => {
+router.route("/user").get((req, res) => {
   Score.find()
     .then((user) => {
-      res.json(user)
+      res.json(user);
     })
     .catch((err) => {
-      res.status(400).json('Erro' + err)
+      res.status(400).json("Erro" + err);
+    });
+});
+
+router.route("/createScore").post((req, res) => {
+  const username = req.body.username;
+  const score = req.body.score;
+  const temp = new Score({
+    username,
+    score,
+  });
+
+  console.log("Landed");
+  temp
+    .save()
+    .then(() => {
+      res.json("Yay you just created a new one!");
     })
-})
+    .catch((err) => {
+      res.status(400).json("Error" + err);
+    });
+});
 
-router.route('/createScore').post((req, res) => {
-    const username = req.body.username
-    const score = req.body.score
-    const temp = new Score({
-        username,
-        score,
+router.route("/getTop10").get((req, res) => {
+  Score.find({}, "username score")
+    .sort({ score: -1 })
+    .limit(10)
+    .then((topScores) => {
+      res.json(topScores);
     })
+    .catch((err) => {
+      res.status(400).json("Error: " + err);
+    });
+});
 
-    console.log("Landed")
-    temp.save()
-        .then(() => {
-          res.json("Yay you just created a new one!")
-        })
-        .catch((err) => {
-          res.status(400).json("Error" + err)
-        })
-})
-
-router.route('/delete/:id').delete((req, res) => {
+router.route("/delete/:id").delete((req, res) => {
   Score.findByIdAndDelete(req.params.id)
-    .then((exercise)=>{
-      res.json(exercise)
+    .then((exercise) => {
+      res.json(exercise);
     })
     .catch((err) => {
-      res.status(400).json(err)
-    })
-})
+      res.status(400).json(err);
+    });
+});
 
-module.exports = router
+module.exports = router;
